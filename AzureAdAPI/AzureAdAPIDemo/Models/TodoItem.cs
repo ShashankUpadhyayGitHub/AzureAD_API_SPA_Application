293cf704576ed63e809c26b78df512f3fb1b32e5
Shashank Upadhyay <shashankupadhyay96@gmail.com>
2023-10-19 23:17:17 +0530

    Azure AD with web API and Angular application

---
+++ AzureAdAPI/AzureAdAPIDemo/Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAdAPIDemo.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
